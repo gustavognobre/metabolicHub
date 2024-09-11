2287e0672b069990f614839b7cd3e3d736307585
Gustavo Nobre <gustavognobre@gmail.com>
2024-09-10 22:07:50 -0300

    cardWrapper

--- components/auth/cardwrapper/card-wrapper.tsx
+++ components/auth/cardwrapper/card-wrapper.tsx
@@ -3,39 +3,38 @@
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import { HeaderCard } from "@/components/auth/cardwrapper/header";
 import { Social } from "@/components/auth/cardwrapper/social";
+import { BackButton } from "@/components/auth/cardwrapper/back-button";
 
 interface CardWrapperProps {
-    children: React.ReactNode;
-    headerLabel: string;
-    backButtonLabel: string;
-    backButtonHref: string;
-    showSocial?: boolean;
+  children: React.ReactNode;
+  showTabs?: boolean;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
 
-};
 export const CardWrapper = ({
-    children,
-    headerLabel,
-    backButtonLabel,
-    backButtonHref,
-    showSocial,
-
+  children,
+  headerLabel,
+  backButtonLabel,
+  backButtonHref,
+  showSocial,
 }: CardWrapperProps) => {
-    return (
-
-        <Card >
-            <CardHeader>
-                <HeaderCard label={headerLabel}/>
-            </CardHeader>
-            <CardContent>
-                {children}
-            </CardContent>
-            {showSocial &&(
-                <CardFooter>
-                    <Social/>
-                </CardFooter>
-            )}
-                
-
-        </Card>
-    );
+  return (
+    <Card className="w-full max-w-md mx-auto sm:max-w-lg md:max-w-xl lg:max-w-2xl xl:max-w-3xl p-4">
+      <CardHeader>
+        <HeaderCard label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel} />
+      </CardFooter>
+    </Card>
+  );
 };
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,11 +1,14 @@
 import { CardWrapper } from "@/components/auth/cardwrapper/card-wrapper";
 
 export default function LoginForm() {
-  return <CardWrapper
-            headerLabel="Bem Vindo"
-            backButtonHref="/auth/register"
-            backButtonLabel="Não tem uma conta?"
-            showSocial>
-                Formulário de Login
-        </CardWrapper>
+  return (
+    <CardWrapper
+      headerLabel="Bem Vindo"
+      backButtonHref="/auth/register"
+      backButtonLabel="Não tem uma conta?"
+      showSocial
+    >
+      Formulário de Login
+    </CardWrapper>
+  );
 }
